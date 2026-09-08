9e0cb095e3a5a8dfd72f2605e569fa28f312b433
ncsavilla-ctrl <ncsavilla@brc.pshs.edu.ph>
2026-09-08 20:51:23 +0800

    Refactor displayInfo method and update stream formatting
    Refactor displayInfo method to remove selected_artist parameter and format stream output with commas.

--- q1/classImplementation.py
+++ q1/classImplementation.py
@@ -5,13 +5,13 @@ class MusicGenre:
         self.__artist = artist
         self._streams = streams
 
-    def displayInfo(self, selected_artist):
-            if self.__artist == selected_artist:
-            print(f"Title: {self.title}, Genre:{self.genre}, Artist:{self.__artist}, Streams:{self._streams}")
+    def displayInfo(self):
+        print(f"Title: {self.title}, Genre: {self.genre}, "
+              f"Artist: {self.__artist}, Streams: {self._streams:,}")
 
     def updateStreams(self, amount):
         self._streams += amount
-        print(f"Updated streams for {self.title}: {self._streams}")
+        print(f"Updated streams for {self.title}: {self._streams:,}")
 
     def displayGenre(self, selected_song):
         print(f"Genre of {selected_song.title}: {selected_song.genre}")
@@ -19,21 +19,32 @@ class MusicGenre:
     def displayTitle(self, selected_song):
         print(f"Title of the song: {selected_song.title}")
 
-object1 = MusicGenre("La La Lost You", "indie pop", "NIKI", "585,000,000")
-object2 = MusicGenre("Mahika", "OPM", "Adie", "446,000,000")
-object3 = MusicGenre("Fortnight", "synth-pop", "Taylor Swift", "1,239,082,552")
 
-MusicGenre.displayInfo(object1)
-MusicGenre.displayTitle(object2)
-MusicGenre.MusicGenre(object3)
+object1 = MusicGenre("La La Lost You", "indie pop", "NIKI", 585000000)
+object2 = MusicGenre("Mahika", "OPM", "Adie", 446000000)
+object3 = MusicGenre("Fortnight", "synth-pop", "Taylor Swift", 1239082552)
 
 print("--- BEFORE ---")
-print("Object 1: displayInfo")
-print("Object 2: displayTitle")
-print("Object 3: MusicGenre")
 
-print("Changing streams of object 1...")
-print("--- AFTER ---")
 print("Object 1: displayInfo")
+object1.displayInfo()
+
 print("Object 2: displayTitle")
-print("Object 3: MusicGenre")
+object2.displayTitle(object2)
+
+print("Object 3: updateStreams")
+object3.updateStreams(1000000)
+
+print("\nChanging streams of object 1...")
+object1.updateStreams(1000000)
+
+print("\n--- AFTER ---")
+
+print("Object 1: displayInfo")
+object1.displayInfo()
+
+print("Object 2: displayTitle")
+object2.displayTitle(object2)
+
+print("Object 3: updateStreams")
+object3.updateStreams(1000000)
