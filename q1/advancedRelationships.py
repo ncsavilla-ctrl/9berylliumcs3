1c1699874ecf89ee4632feba5e6ece42aa04f364
ncsavilla-ctrl <ncsavilla@brc.pshs.edu.ph>
2026-09-24 20:39:28 +0800

    Update advancedRelationships.py

--- q1/advancedRelationships.py
+++ q1/advancedRelationships.py
@@ -1,9 +1,22 @@
-class ParentClass:
-def __init__(self, value):
-self.value = value
+class AudioTrack:
+  def __init__(self, title: str, duration: int, id: str):
+    self.id = id
+    self.title = title
+    self.duration = duration
 
-class ChildClass(ParentClass):
+  def display_info(self):
+    mins = self.duration // 60
+    secs = self.duration % 60
+  return f"ID: {self.id} | Title: {self.title} | Duration: {mins}:{secs:02d}"
+  
+class Song(AudioTrack):
+  def __init__(self, id: str, title: str, duration: int, lyrics: str, bpm: int):
+    super().__init__(id, title, duration)
+    self.lyrics = lyrics
+    self.bpm = bpm
+    self.duration = duration
+    self.title = title
 
-def __init__(self, value, extra):
-super().__init__(value)
-self.extra = extra
+  def get_song_details(self):
+    return f"{self.display_info()} | BPM: {self.bpm}\nLyrics Snippet: 'self.lyrics[:30]}...'"
+    
