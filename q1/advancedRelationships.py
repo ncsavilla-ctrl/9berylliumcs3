cbaae8f403092f8c0c06d56e2396ed1d7c6dc077
ncsavilla-ctrl <ncsavilla@brc.pshs.edu.ph>
2026-09-24 19:23:06 +0800

    Update advancedRelationships.py

--- q1/advancedRelationships.py
+++ q1/advancedRelationships.py
@@ -1,1 +1,9 @@
+class ParentClass:
+def __init__(self, value):
+self.value = value
 
+class ChildClass(ParentClass):
+
+def __init__(self, value, extra):
+super().__init__(value)
+self.extra = extra
